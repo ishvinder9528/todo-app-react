2f0fd190d052c9f64f0ff80f63e892bb3fb319d9
ishvinder9528 <ishvinder133@gmail.com>
2023-02-04 14:58:20 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import TodoList from "./Component/TodoList";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
